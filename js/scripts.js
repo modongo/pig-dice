6c35ceff13f0ec9642f2d183601c0f15ded43a00
Michael Odongo <mikodongo@gmail.com>
2019-04-12 19:33:57 +0300

    updated the working files

---
+++ js/scripts.js
@@ -0,0 +1,92 @@
+var throwdice = function () {
+  return Math.floor(6 * Math.random()) + 1;
+}
+
+function Player() {
+  this.roll = 0;
+  this.tempscore = 0;
+  this.totalscore = 0;
+  this.turn = 1;
+}
+
+// checking if roll value is one
+Player.prototype.rollone = function () {
+  if (this.roll === 1) {
+    this.tempscore = 0;
+    alert("Sorry , you rolled a 1! Your turn is over!");
+  } else {
+    this.tempscore += this.roll;
+  }
+}
+
+// player selects to hold 
+Player.prototype.hold = function () {
+  this.totalscore += this.tempscore;
+  this.tempscore = 0;
+  // this.changeturn();
+  alert(this.playerName + ", your turn is over, pass the mouse!");
+}
+
+
+Player.prototype.winnerCheck = function () {
+  if (this.totalscore >= 100) {
+    alert(this.playerName + " You are the winner!");
+  }
+}
+
+
+// User Interface
+$(document).ready(function () {
+
+
+  player1 = new Player();
+  player2 = new Player();
+  $("#player1-panel").show();
+  $("#player2-panel").hide();
+
+
+  $("button#player1-roll").click(function (event) {
+    player1.roll = throwdice();
+    $("#roll-score-player1").text(player1.roll);
+    player1.rollone();
+    if (player1.tempscore === 0) {
+      // alert(this.turn);
+      $("#player1-panel").hide();
+      $("#player2-panel").show();
+    }
+    $("#roll-totals-player1").text(player1.tempscore);
+  });
+
+  $("button#player2-roll").click(function (event) {
+    player2.roll = throwdice();
+    $("#roll-score-player2").text(player2.roll);
+    player2.rollone();
+    if (player2.tempscore === 0) {
+      $("#player2-panel").hide();
+      $("#player1-panel").show();
+    }
+    $("#roll-totals-player2").text(player2.tempscore);
+  });
+
+  $("button#player1-hold").click(function (event) {
+    player1.hold();
+    $("#final-totals-player1").text(player1.totalscore);
+    $("#roll-score-player1").empty();
+    $("#roll-totals-player1").empty();
+    $("#player1-panel").hide();
+    $("#player2-panel").show();
+
+    player1.winnerCheck();
+  });
+
+  $("button#player2-hold").click(function (event) {
+    player2.hold();
+    $("#final-totals-player2").text(player2.totalscore);
+    $("#roll-score-player2").empty();
+    $("#roll-totals-player2").empty();
+    $("#player2-panel").hide();
+    $("#player1-panel").show();
+    player2.winnerCheck();
+  });
+
+});
